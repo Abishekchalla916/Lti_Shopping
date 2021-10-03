20c7ab9776fd16158799dcf31e1c5f4e0b3a4545
Abishekchalla916 <90753182+Abishekchalla916@users.noreply.github.com>
2021-10-03 11:04:31 +0530

    Add files via upload

--- LtiProduct/layer5/LTIProductJPAController.java
+++ LtiProduct/layer5/LTIProductJPAController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,11 +21,18 @@ import org.springframework.web.context.request.WebRequest;
 
 import com.example.demo.layer3.LTIProductRepository;
 import com.example.demo.layer4.LTIProductService;
+import com.example.demo.layer4.LTIRetailerService;
+import com.example.demo.Exception.LtiProductNotFoundException;
+import com.example.demo.Exception.RetailerNotFoundExcepiton;
 import com.example.demo.layer2.model.LtiProduct;
+import com.example.demo.layer2.model.LtiRetailer;
 @CrossOrigin
 @RestController
+@Service
 public class LTIProductJPAController {
 
+	@Autowired
+	LTIRetailerService retailerService;
 	
 	@Autowired
 	LTIProductService prodService; //here it should be service calls
@@ -57,35 +65,65 @@ public class LTIProductJPAController {
 	}
 	
 	@PostMapping
-	@RequestMapping(path="/addJPAProd") // 
-	public void addDepartment2(@RequestBody LtiProduct deptToInsert) throws LtiProductNotFoundException 
+	@RequestMapping(path="/addJPAProd/{retilerID}")
+	public void addProduct(@RequestBody LtiProduct prodToInsert,@PathVariable("retilerID")long retailerId) throws LtiProductNotFoundException,RetailerNotFoundExcepiton 
 	{
-		//System.out.println("addDepartment2 : " + deptToInsert.getDname()+" "+deptToInsert.getLoc()); 
-		System.out.println("Adding department");
-		prodService.insertProduct(deptToInsert);
-		//deptRepo.insertDept(deptToInsert);
+	
+		//System.out.println("addDepartment2 : " + deptToInsert.getDname()+" "+deptToInsert.getLoc());
+		LtiRetailer retailer=retailerService.selectLtiRetailerByRetailerId(retailerId);
+		if(retailer!=null) {
+			prodService.insertProduct(prodToInsert);
+			prodToInsert.setLtiRetailer(retailer);
+			prodService.updateProduct(prodToInsert);
+			List<LtiProduct> prodList=new ArrayList<>();
+			prodList.add(prodToInsert);
+			retailer.setLtiProducts(prodList);
+			retailerService.updateRetailer(retailer);
+			
+//			List<LtiProduct> prodList=retailer.getLtiProducts();
+//			prodList.add(prodToInsert);
+//			retailer.setLtiProducts(prodList);
+//			retailerService.updateRetailer(retailer);			
+			//prodToInsert.setLtiRetailer(retailer);			
+		}else {
+			RetailerNotFoundExcepiton ex=new RetailerNotFoundExcepiton("Retailer dosen't exists");
+			throw ex;
+		}
+		System.out.println("Adding Product ");
+		System.out.println(prodToInsert.getProdBrand());
+		//System.out.println(prodToInsert.getLtiRetailer().getRetailerId());
+		System.out.println(prodToInsert.getProdName());
+		System.out.println(prodToInsert.getProdPrice());
+		System.out.println(prodToInsert.getStocksRemaining());
+		System.out.println(prodToInsert.getProdDescription());
+		prodService.insertProduct(prodToInsert);
 		
 	}
 	
 	@PostMapping
-	@RequestMapping(path="/modifyJPAProd") // 
-	public void modifyDepartment(@RequestBody LtiProduct prodToModify) throws LtiProductNotFoundException 
+	@RequestMapping(path="/modifyJPAProdStocks/{noOfStocksToReduce}") //
+	public void updateProduct(@PathVariable ("noOfStocksToReduce")int noOfStocksToReduce, @RequestBody LtiProduct prodToModify) throws LtiProductNotFoundException 
 	{
-		//System.out.println("modifyDepartment : "+deptToModify.getDeptno()+" "+deptToModify.getDname()+" "+deptToModify.getLoc()); 
+		if(noOfStocksToReduce<-1) throw new RuntimeException("Invalid number to reduce stocks");
 		System.out.println("Modifying....");
-		boolean found=false;
-		prodService.updateProduct(prodToModify);
+		if(prodToModify==null) throw new RuntimeException("Invalid product to modify");
+		LtiProduct prod=prodService.selectProductByProdId(prodToModify.getProdId());
+		if(prod==null) throw new LtiProductNotFoundException("Product not found ");
+		prod.setStocksRemaining(prod.getStocksRemaining()-(noOfStocksToReduce==-1?prod.getStocksRemaining():noOfStocksToReduce));
+		prodService.updateProduct(prod);
 	}
-	@PostMapping
-	@RequestMapping(path="/deleteJPAProd") // localhost:8080/deleteDept/10
-	public void deleteDepartment(@RequestBody LtiProduct prod) throws LtiProductNotFoundException 
+	
+	@GetMapping
+	@RequestMapping(path="/deleteJPAProd/{prodId}") // localhost:8080/deleteDept/10
+	public String deleteProduct(@PathVariable("prodId")  String prodId) throws LtiProductNotFoundException 
 	{
 		//System.out.println("deleteDepartment : "+dept.getDeptno());
 		try {
-			prodService.deleteProduct(1525);
+			prodService.deleteProduct(Long.valueOf(prodId));
+			return "Prod deleted";
 		}catch(Exception e) {
 						
 		}
+		return null;
 	}
-		
 }
